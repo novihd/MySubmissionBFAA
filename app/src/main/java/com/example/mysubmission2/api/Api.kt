3e8a03f7f159d62c364ed34721a4e1f363ccb734
novi <novihd@aol.com>
2021-11-01 14:23:00 +0700

    delete api

--- app/src/main/java/com/example/mysubmission2/api/Api.kt
+++ app/src/main/java/com/example/mysubmission2/api/Api.kt
@@ -11,25 +11,21 @@ import retrofit2.http.Query
 
 interface Api {
     @GET("search/users")
-    @Headers("Authorization: token c058d33d6a02b8cab9e8cafeb6f65a44839d623f")
     fun getSearchUser(
         @Query("q") query: String
     ): Call<UserResponse>
 
     @GET("users/{username}")
-    @Headers("Authorization: token c058d33d6a02b8cab9e8cafeb6f65a44839d623f")
     fun getUserDetail(
         @Path("username") username : String
     ): Call<DetailUserResponse>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token c058d33d6a02b8cab9e8cafeb6f65a44839d623f")
     fun getFollowers(
         @Path("username") username : String
     ): Call<ArrayList<UserModel>>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token c058d33d6a02b8cab9e8cafeb6f65a44839d623f")
     fun getFollowing(
         @Path("username") username : String
     ): Call<ArrayList<UserModel>>
